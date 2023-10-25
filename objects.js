ec8682bfe87396378f646169dcc73acda378d599
Thamilanpu <sthamil1016@gmail.com>
2023-10-25 13:15:35 +0530

    pair work

---
+++ objects.js
@@ -0,0 +1,42 @@
+// q1
+let x={}
+console.log(x)
+
+// q2
+let person={
+    name:"Thamilanpu",
+    age:21,
+    gender:"male"
+    
+}
+console.log("Hello, my name is "+person.name);
+
+// q3
+function greet(greetstudent) {
+    return "Hello"+person.name+"welcome to the coding school"
+}
+console.log(greet("greetstudent"));
+
+// q4
+console.log("Name:", person.name);
+console.log("Name:", person["name"]);
+// q5
+person.email="john@example.com"
+console.log(person.email);
+
+// q6
+delete person.gender;
+console.log(person);
+
+//q7
+
+
+//q8
+let student={
+    name:"Thamilanpu"
+}
+let courses ={
+    course:"Web Development"
+}
+let studentcourse = Object.assign({}, student, courses);
+console.log(studentcourse);
